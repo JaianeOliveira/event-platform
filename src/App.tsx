fe9ace4c973777cc6821aee0426db0f9f784f6f4
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-06-24 00:07:55 -0300

    feat: base components files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
-import { gql, useQuery } from '@apollo/client';
+import { Header, Sidebar, Video, Lesson } from './components';
 
-const GET_LESSONS_QUERY = gql`
-	query lessons {
-		lessons {
-			id
-			title
-		}
-	}
-`;
-
-type Lesson = {
-	id: string;
-	title: string;
-};
-
-const App = () => {
-	const { data, loading } = useQuery(GET_LESSONS_QUERY);
-
-	if (loading) {
-		return <h1>Carregando</h1>;
-	}
-	return (
-		<div>
-			<ul>
-				{data?.lessons.map((lesson: Lesson) => (
-					<li key={lesson.id}>{lesson.title}</li>
-				))}
-			</ul>
-		</div>
-	);
-};
-
+const App = () => (
+	<>
+		<Header />
+		<Sidebar />
+		<Video />
+		<Lesson />
+	</>
+);
 export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Lesson from './Lesson';
+import Video from './Video';
+
+export { Header, Sidebar, Lesson, Video };
