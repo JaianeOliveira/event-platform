007728517248466de82fe0e290bfcd9e13d3bc6c
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2022-06-23 23:52:22 -0300

    feat: setup config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,34 @@
-export default () => {
-	return <h1 className="text-4xl">Hellow</h1>;
+import { gql, useQuery } from '@apollo/client';
+
+const GET_LESSONS_QUERY = gql`
+	query lessons {
+		lessons {
+			id
+			title
+		}
+	}
+`;
+
+type Lesson = {
+	id: string;
+	title: string;
 };
+
+const App = () => {
+	const { data, loading } = useQuery(GET_LESSONS_QUERY);
+
+	if (loading) {
+		return <h1>Carregando</h1>;
+	}
+	return (
+		<div>
+			<ul>
+				{data?.lessons.map((lesson: Lesson) => (
+					<li key={lesson.id}>{lesson.title}</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default App;
